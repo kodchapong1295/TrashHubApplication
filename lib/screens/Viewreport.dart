89d62e3f74dd5a6c58d5548f7727ced89fd58ef4
Gorgun <yosathorn.pengma@mail.kmutt.ac.th>
2021-05-10 16:39:46 +0700

    [DELETE] ViewReport

--- lib/screens/Viewreport.dart
+++ lib/screens/Viewreport.dart
@@ -16,46 +16,7 @@ class _ViewReportState extends State<ViewReport> {
           child: Container(
             child: Column(
               children: <Widget>[
-                SizedBox(
-                    height: height,
-                    child: Stack(children: <Widget>[
-                      Container(
-                          child: SizedBox(
-                        width: width,
-                        height: 170,
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(color: Colors.white),
-                          child: Stack(
-                            children: <Widget>[
-                              Positioned(
-                                top: height * (9 / 100),
-                                child: Row(
-                                  children: [
-                                    Container(
-                                      margin: new EdgeInsets.only(right: 45),
-                                      width: width * 0.2,
-                                      child: IconButton(
-                                        icon: Icon(Icons.arrow_back,
-                                            color: Colors.green),
-                                        onPressed: () =>
-                                            {Navigator.of(context).pop(false)},
-                                      ),
-                                    ),
-                                    Text(
-                                      'ตากผ้าแปบ',
-                                      style: TextStyle(
-                                          color: Colors.green,
-                                          fontSize: 24,
-                                          fontFamily: 'Open-Sans'),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ))
-                    ])),
+              
               ],
             ),
           ),
