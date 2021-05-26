c4cdc59903759af042df9de000d0c208514f649b
best <sunsasabest@hotmail.com>
2021-05-26 14:37:07 +0700

    [ADD] Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,58 @@ import 'package:trashhub/screens/AuthenMenu.dart';
 import 'package:trashhub/screens/UserProfile.dart';
 import 'package:trashhub/constants.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Open Sans',
-        primaryColor: Colors.white,
-        textTheme: TextTheme(
-          headline1: TextStyle(
-            fontSize: 48,
-            fontWeight: FontWeight.bold,
-            color: kPrimaryColor,
-          ),
-          bodyText1: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.normal,
-            color: kPrimaryColor,
-          ),
-        ),
-      ),
-      home: AuthenMenu(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          print('ERROR');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              fontFamily: 'Open Sans',
+              primaryColor: Colors.white,
+              textTheme: TextTheme(
+                headline1: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: kPrimaryColor,
+                ),
+                bodyText1: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                  color: kPrimaryColor,
+                ),
+              ),
+            ),
+            home: AuthenMenu(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        // return Loading();
+      },
     );
   }
 }
