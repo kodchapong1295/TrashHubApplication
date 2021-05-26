406eadd4d9074b281080aec201cea12b2440ed08
best <sunsasabest@hotmail.com>
2021-05-27 02:21:36 +0700

    [ADD] image picker

--- lib/screens/GeneralUserScreen/UserRegisterScreen.dart
+++ lib/screens/GeneralUserScreen/UserRegisterScreen.dart
@@ -1,16 +1,38 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:trashhub/screens/GeneralUserScreen/UserProfile.dart';
 import 'package:trashhub/constants.dart';
 import 'package:trashhub/components/RoundedButton.dart';
 import 'package:trashhub/Firebase.dart';
 import 'package:provider/provider.dart';
 
-class UserRegisterScreen extends StatelessWidget {
+class UserRegisterScreen extends StatefulWidget {
+  @override
+  _UserRegisterScreenState createState() => _UserRegisterScreenState();
+}
+
+class _UserRegisterScreenState extends State<UserRegisterScreen> {
   String email;
   String password;
   String firstname;
   String lastname;
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,6 +72,26 @@ class UserRegisterScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      child: _image == null
+                          ? MaterialButton(
+                              onPressed: getImage,
+                              color: kPrimaryColor,
+                              textColor: Colors.white,
+                              child: Icon(
+                                Icons.camera_alt,
+                                size: 40,
+                              ),
+                              padding: EdgeInsets.all(16),
+                              shape: CircleBorder(),
+                            )
+                          : Image.file(_image),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
                     TextFormField(
                       onChanged: (fn) => {this.firstname = fn},
                       decoration: kTextField.copyWith(hintText: 'Firstname'),
