0b7ffe0905f4968fddeda78f0d7a07ef9642b73c
best <sunsasabest@hotmail.com>
2021-05-28 01:22:55 +0700

    [FIX] reload authen loss

--- lib/screens/AuthenMenu.dart
+++ lib/screens/AuthenMenu.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:trashhub/constants.dart';
 
@@ -16,104 +18,227 @@ import 'package:trashhub/Firebase.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthenMenu extends StatelessWidget {
+  Future<bool> isUser(context) async {
+    final temp = await (context).read<FlutterFireAuthService>().isUser();
+    return temp;
+  }
+
   @override
   Widget build(BuildContext context) {
     final firebaseUser = context.watch<User>();
-    if (firebaseUser != null) {
-      print('User has already logged in');
-      return UserProfileScreen();
-    }
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  Container(
-                    child: Image.asset('images/logo.png'),
-                    width: 184,
+    return firebaseUser != null
+        ? FutureBuilder<String>(future: Future<String>.sync(() async {
+            final isUser =
+                await (context).read<FlutterFireAuthService>().isUser();
+
+            if (isUser) {
+              return "User";
+            }
+            return "NGO";
+          }), builder: (ctx, auth) {
+            print(auth);
+            if (auth.hasData) {
+              if (auth.data == "User") {
+                return UserProfileScreen();
+              } else {
+                return NGOProfileScreen();
+              }
+            } else {
+              return Scaffold(
+                body: SafeArea(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              child: Image.asset('images/logo.png'),
+                              width: 184,
+                            ),
+                            Text(
+                              'TRASH HUB',
+                              style: Theme.of(context).textTheme.headline1,
+                            ),
+                            Text(
+                              'Trash Problem Notifier',
+                              style: Theme.of(context).textTheme.bodyText1,
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              RoundedButton(
+                                title: "LOG IN",
+                                btnColor: kPrimaryColor,
+                                textColor: Colors.white,
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            UserLoginScreen()),
+                                  );
+                                },
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              RoundedButton(
+                                title: "SIGN UP",
+                                btnColor: kPrimaryColor,
+                                textColor: Colors.white,
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            UserRegisterScreen()),
+                                  );
+                                },
+                              ),
+                              TextButton(
+                                child: Text(
+                                  'Login as NGOs',
+                                  style: Theme.of(context).textTheme.bodyText1,
+                                  textAlign: TextAlign.center,
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            NGOsLoginScreen()),
+                                  );
+                                },
+                              ),
+                              TextButton(
+                                child: Text(
+                                  'Do not press it !!!!',
+                                  style: Theme.of(context).textTheme.bodyText1,
+                                  textAlign: TextAlign.center,
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            NGOProfileScreen()),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  Text(
-                    'TRASH HUB',
-                    style: Theme.of(context).textTheme.headline1,
-                  ),
-                  Text(
-                    'Trash Problem Notifier',
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
+                ),
+              );
+            }
+          })
+        : Scaffold(
+            body: SafeArea(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    RoundedButton(
-                      title: "LOG IN",
-                      btnColor: kPrimaryColor,
-                      textColor: Colors.white,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => UserLoginScreen()),
-                        );
-                      },
+                    Column(
+                      children: [
+                        Container(
+                          child: Image.asset('images/logo.png'),
+                          width: 184,
+                        ),
+                        Text(
+                          'TRASH HUB',
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
+                        Text(
+                          'Trash Problem Notifier',
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    RoundedButton(
-                      title: "SIGN UP",
-                      btnColor: kPrimaryColor,
-                      textColor: Colors.white,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => UserRegisterScreen()),
-                        );
-                      },
-                    ),
-                    TextButton(
-                      child: Text(
-                        'Login as NGOs',
-                        style: Theme.of(context).textTheme.bodyText1,
-                        textAlign: TextAlign.center,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          RoundedButton(
+                            title: "LOG IN",
+                            btnColor: kPrimaryColor,
+                            textColor: Colors.white,
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => UserLoginScreen()),
+                              );
+                            },
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          RoundedButton(
+                            title: "SIGN UP",
+                            btnColor: kPrimaryColor,
+                            textColor: Colors.white,
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => UserRegisterScreen()),
+                              );
+                            },
+                          ),
+                          TextButton(
+                            child: Text(
+                              'Login as NGOs',
+                              style: Theme.of(context).textTheme.bodyText1,
+                              textAlign: TextAlign.center,
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => NGOsLoginScreen()),
+                              );
+                            },
+                          ),
+                          TextButton(
+                            child: Text(
+                              'Do not press it !!!!',
+                              style: Theme.of(context).textTheme.bodyText1,
+                              textAlign: TextAlign.center,
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => NGOProfileScreen()),
+                              );
+                            },
+                          ),
+                        ],
                       ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => NGOsLoginScreen()),
-                        );
-                      },
-                    ),
-                    TextButton(
-                      child: Text(
-                        'Do not press it !!!!',
-                        style: Theme.of(context).textTheme.bodyText1,
-                        textAlign: TextAlign.center,
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => NGOProfileScreen()),
-                        );
-                      },
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
+    // if (firebaseUser != null) {
+    //   print(firebaseUser);
+    //   print('User has already logged in');
+    //   return UserProfileScreen();
+    // }
+    // return
   }
 }
