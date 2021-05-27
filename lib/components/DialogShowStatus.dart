d13873ff29155e34a9bd0fde80eebe63579d8f48
Gorgun <yosathorn.pengma@mail.kmutt.ac.th>
2021-05-27 23:22:12 +0700

    [Add]DialogShowStatus

---
+++ lib/components/DialogShowStatus.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:trashhub/components/RoundedButton.dart';
+import 'package:trashhub/constants.dart';
+
+class DialogShowStatus extends StatelessWidget {
+  final String topic, title, description, button, location, date, status;
+
+  final String no;
+
+  DialogShowStatus(
+      {this.topic,
+      this.description,
+      this.button,
+      this.no,
+      this.date,
+      this.title,
+      this.status,
+      this.location});
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: dialogContent(context),
+    );
+  }
+
+  Widget dialogContent(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: height * .6,
+          padding: EdgeInsets.only(
+            top: 35,
+            bottom: 16,
+            left: 6,
+            right: 16,
+          ),
+          margin: EdgeInsets.only(top: 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(17),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                offset: Offset(0, 10),
+              )
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: width,
+                child: Text(
+                  topic,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 35),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "No    : ",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: kPrimaryColor,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          no.toString(),
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: kPrimaryColor,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Date : ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          date,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: kPrimaryColor,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      "Location : ",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      location,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: kPrimaryColor,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      "Description :",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      description,
+                      style: TextStyle(fontSize: 16, color: kPrimaryColor),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 110),
+              Center(
+                child: Container(
+                  width: 200,
+                  child: RoundedButton(
+                    title: "Accept",
+                    btnColor: kPrimaryColor,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
