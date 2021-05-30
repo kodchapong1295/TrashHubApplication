753df43f9886edd84367d87fdbab0660b0d21b0c
Gorgun <yosathorn.pengma@mail.kmutt.ac.th>
2021-05-30 16:50:20 +0700

    [ADD] DialogFor Obgoing

---
+++ lib/components/ViewReport/DialogShowOngoing.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'package:trashhub/components/RoundedButton.dart';
+import 'package:trashhub/constants.dart';
+
+class DialogShowOngoing extends StatelessWidget {
+  final String topic,
+      title,
+      description,
+      button,
+      location,
+      date,
+      status,
+      responsible_by;
+
+  final String id;
+
+  DialogShowOngoing(
+      {this.topic,
+      this.responsible_by,
+      this.description,
+      this.button,
+      this.id,
+      this.date,
+      this.title,
+      this.status,
+      this.location});
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return AlertDialog(
+      scrollable: true,
+      title: Text(
+        topic,
+        style: TextStyle(
+          fontSize: 24,
+          color: kPrimaryColor,
+          fontWeight: FontWeight.w700,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 0,
+      // backgroundColor: Colors.transparent,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 24,
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(25, 0, 25, 25),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "ID: ",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: kPrimaryColor,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          id.toString(),
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: kPrimaryColor,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Date : ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          date,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: kPrimaryColor,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Status : ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "${status[0].toUpperCase()}${status.substring(1)}",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: kPrimaryColor,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      "Responsible NGO :",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      responsible_by == "" ? "None" : responsible_by,
+                      style: TextStyle(fontSize: 16, color: kPrimaryColor),
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      "Location : ",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      location,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: kPrimaryColor,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      "Description :",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      description == "" ? "None" : description,
+                      style: TextStyle(fontSize: 16, color: kPrimaryColor),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          // Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          //   Container(
+          //     margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          //     width: 150,
+          //     child: RoundedButton(
+          //       title: "Okay",
+          //       btnColor: kPrimaryColor,
+          //       textColor: Colors.white,
+          //       onPressed: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //   ),
+          // ])
+        ],
+      ),
+      actions: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          width: 100,
+          child: TextButton(
+            child: Text("Complete",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.bold)),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
